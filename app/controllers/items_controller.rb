3b5b5fd9259da19523a6d8591a63950c8ef87568
kylejb <kjburda@gmail.com>
2020-09-03 11:31:55 -0400

    Paired Programming: User&Item Controllers

---
+++ app/controllers/items_controller.rb
@@ -0,0 +1,25 @@
+class ItemsController < ApplicationController
+
+    before_action :find_item, except: [:index, :new, :edit]
+
+    def index
+        @items = Item.all
+
+        render json: @items
+    end
+
+    def show
+        render json: @item
+    end
+
+    private
+
+    def item_params
+        params.require(:items).permit(:name, :img_url, :cost, :description)
+    end
+
+    def find_item
+        @item = Item.find(params[:id])
+    end
+end
+
---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,24 @@
+class UsersController < ApplicationController
+
+    before_action :find_user, except: [:index, :new, :edit]
+
+    def index
+        @users = User.all
+
+        render json: @users
+    end
+
+    def show
+        render json: @user
+    end
+
+    private
+
+    def user_params
+        params.require(:users).permit(:name, :email, :password, :password_confirmation)
+    end
+
+    def find_user
+        @user = User.find(params[:id])
+    end
+end

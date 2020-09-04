73c84ef1f58bc802597d22daa2f4c36cffc60cb8
kylejb <kjburda@gmail.com>
2020-09-03 20:57:49 -0400

    Refactored Application and Users Controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,22 +2,11 @@ class ApplicationController < ActionController::API
     before_action :authorized
 
     def encode_token(payload)
-        JWT.encode(payload, Rails.application.credentials.jwt_api[:secret])
+        JwtAuth::encode_token(payload)
     end
      
-    def auth_header
-    request.headers['Authorization']
-    end
-     
-    def decoded_token
-        if auth_header
-            token = auth_header.split(' ')[1]
-            begin
-            JWT.decode(token, Rails.application.credentials.jwt_api[:secret], true, algorithm: 'HS256')
-            rescue JWT::DecodeError
-            nil
-            end
-        end
+    def decoded_token(headers = request.headers['Authorization'])
+        JwtAuth::decoded_token(headers)
     end
      
     def current_user
@@ -26,12 +15,12 @@ class ApplicationController < ActionController::API
             @user = User.find_by(id: user_id)
         end
     end
-     
+    
     def logged_in?
         !!current_user
     end
-
+    
     def authorized
-        render json: { message: 'Please log in' }, status: :unauthorized unless logged_in?
+        render json: { message: 'Please log in' }, status: :unauthorized unless self.logged_in?
     end
 end

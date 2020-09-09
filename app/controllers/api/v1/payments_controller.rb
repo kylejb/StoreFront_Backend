062bc0d6848af13f92008ffdcf2fcb938b86a17f
kylejb <kjburda@gmail.com>
2020-09-09 16:13:43 -0400

    Refactored to enable current_user method

--- app/controllers/api/v1/payments_controller.rb
+++ app/controllers/api/v1/payments_controller.rb
@@ -3,17 +3,24 @@ class Api::V1::PaymentsController < ApplicationController
     skip_before_action :authorized, only: [:create]
 
     def create
+        # TODO - make sure Auth grabs user from token in client-side request; either through controller logic or decoding the request token
+        Payment.aggregate_order_from_cart(params[:payment][:order][0][:cart], current_user)
+
         # build payment object for Stripe API through 'payment' model
         # send request to Stripe API through 'stripe_api' model
+        # https://stripe.com/docs/api/charges/object
 
-        render json: { payment_confirmation: "Payment Receipt Object Will Be Passed Here (and/or URL object from Stripe API)"}, status: :created
+        #* build_payment_object(order)
+        #* StripeApi::send_payment(line_15)
+
+        render json: { payment_confirmation: "Payment Receipt Object Will Be Passed Here (and/or URL object from Stripe API)" }, status: :created
     end
-    
+
 
     private
 
     def payment_params
-        params.require(:payment).permit(order: [cart: [:id], billing_address: [:addressLine1, :addressLine2, :city, :state, :zipcode], shipping_address: [:addressLine1, :addressLine2, :city, :state, :zipcode]])
+        params.require(:payment).permit(order: [cart: [:id], billing: [:addressLine1, :addressLine2, :city, :state, :zipcode], shipping: [:addressLine1, :addressLine2, :city, :state, :zipcode]])
     end
 
 end
--- app/models/payment.rb
+++ app/models/payment.rb
@@ -1,6 +1,11 @@
 class Payment < ApplicationRecord
     
-    def build_payment_object(current_order)
-        #TODO - construct Payment Object from Client-side request and pass object through Stripe_Api model
+
+    def self.aggregate_order_from_cart(cart_arr, user_obj)
+        cart_arr.each { | cart_item | PurchaseHistory.create(user: user_obj, item_id: cart_item.id, status: "pending") }
+    end
+
+    def self.build_payment_object(current_order)
+        return { amount: current_order.cost, currency: 'usd', source: 'tok_visa', description: item_description } 
     end
 end

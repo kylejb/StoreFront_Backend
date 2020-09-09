54c6594a1ead181c331fa7d3200164c18f323d18
kylejb <kjburda@gmail.com>
2020-09-08 20:53:05 -0400

    Temp model to construct 'payment objects' for Stripe API; this can be expanded into its own table... or, better yet, combined with current 'items' table to be compliant with Stripe API's payment object requirements while minimizing burden on models/tables

---
+++ app/models/payment.rb
@@ -0,0 +1,6 @@
+class Payment < ApplicationRecord
+    
+    def build_payment_object(current_order)
+        #TODO - construct Payment Object from Client-side request and pass object through Stripe_Api model
+    end
+end

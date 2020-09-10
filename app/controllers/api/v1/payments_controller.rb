4209d7b338bccac7d3c97e5b649c08716e0c706d
kylejb <kjburda@gmail.com>
2020-09-09 21:22:58 -0400

    Wired up Stripe's API endpoint for 'test payments'

--- app/controllers/api/v1/payments_controller.rb
+++ app/controllers/api/v1/payments_controller.rb
@@ -7,20 +7,21 @@ class Api::V1::PaymentsController < ApplicationController
         Payment.aggregate_order_from_cart(params[:payment][:order][0][:cart], current_user)
 
         # build payment object for Stripe API through 'payment' model
+        #? confirm datatype of total when testing... likely an array of float (if value within array is string, convert to float)
+        total_payment = params[:payment][:order][0][:total][0]
+        user_payment_object = build_payment_object(total_payment)
         # send request to Stripe API through 'stripe_api' model
         # https://stripe.com/docs/api/charges/object
+        stripe_response = StripeApi::test_charge(user_payment_object)
 
-        #* build_payment_object(order)
-        #* StripeApi::send_payment(line_15)
-
-        render json: { payment_confirmation: "Payment Receipt Object Will Be Passed Here (and/or URL object from Stripe API)" }, status: :created
+        render json: { payment_confirmation: stripe_response["receipt_url"] }, status: :created
     end
 
 
     private
 
     def payment_params
-        params.require(:payment).permit(order: [cart: [:id], billing: [:addressLine1, :addressLine2, :city, :state, :zipcode], shipping: [:addressLine1, :addressLine2, :city, :state, :zipcode]])
+        params.require(:payment).permit(order: [total: [], cart: [:id], billing: [:addressLine1, :addressLine2, :city, :state, :zipcode], shipping: [:addressLine1, :addressLine2, :city, :state, :zipcode]])
     end
 
 end
--- app/models/stripe_api.rb
+++ app/models/stripe_api.rb
@@ -4,12 +4,7 @@ module StripeApi
     BASE_URL = 'https://api.stripe.com'
     Stripe.api_key = Rails.application.credentials.stripe_test_api[:secret]
 
-    def self.test_charge(amount, item_description)
-        Stripe::Charge.create({
-            amount: amount,
-            currency: 'usd',
-            source: 'tok_visa',
-            description: item_description,
-        })
+    def self.test_charge(payment_obj)
+        Stripe::Charge.create(payment_obj)
     end
 end
